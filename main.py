5636be971885bb3fddb556f8ae967ee001fcf2bc
Kasprian <piotrkasprowicz97@gmail.com>
2021-06-08 09:21:17 +0200

    Zmiana

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ params = [{'hidden_layer_sizes': (10,), 'solver': 'sgd', 'momentum': 0,
            'nesterovs_momentum': False, 'max_iter': 500, 'random_state': 5},
           {'hidden_layer_sizes': (10,), 'solver': 'sgd', 'momentum': 0.9,
            'nesterovs_momentum': True, 'max_iter': 500, 'random_state': 5},
-          {'hidden_layer_sizes': (100,), 'solver': 'sgd', 'momentum': 0,
+          {'hidden_layer_sizes': (15,), 'solver': 'sgd', 'momentum': 0,
            'nesterovs_momentum': False, 'max_iter': 500, 'random_state': 5},
-          {'hidden_layer_sizes': (100,), 'solver': 'sgd', 'momentum': 0.9,
+          {'hidden_layer_sizes': (15,), 'solver': 'sgd', 'momentum': 0.9,
            'nesterovs_momentum': True, 'max_iter': 500, 'random_state': 5},
-          {'hidden_layer_sizes': (500,), 'solver': 'sgd', 'momentum': 0,
+          {'hidden_layer_sizes': (20,), 'solver': 'sgd', 'momentum': 0,
            'nesterovs_momentum': False, 'max_iter': 500, 'random_state': 5},
-          {'hidden_layer_sizes': (500,), 'solver': 'sgd', 'momentum': 0.9,
+          {'hidden_layer_sizes': (20), 'solver': 'sgd', 'momentum': 0.9,
            'nesterovs_momentum': True, 'max_iter': 500, 'random_state': 5}]
 
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
@@ -72,45 +72,43 @@ if __name__ == '__main__':
     y = classification.to_numpy()
 
     # Esperyment
-    results = []
-
-    for param in params:  # po kolei klasyfikatory
-        scores = []
-        clf = MLPClassifier(**param)
-        rkf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=1234)
-        for train_index, test_index in rkf.split(X, y):
-            best = 0
-            for i in range(1, 9):  # 9 pętli dla każdej liczby cech zaczynając od najlepszej według selekcji
+    results = np.zeros((6, 9))
+    for i in range(1, 10):
+        j = 0
+        for param in params:  # po kolei klasyfikatory
+            clf = MLPClassifier(**param)
+            rkf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=1234)
+            scores = []
+            for train_index, test_index in rkf.split(X, y):
                 fit_X, _ = feature_selection(X, y, i)
                 X_train, X_test = fit_X[train_index], fit_X[test_index]
                 y_train, y_test = y[train_index], y[test_index]
                 clf.fit(X_train, y_train)
                 predict = clf.predict(X_test)
-                score = accuracy_score(y_test, predict)
-                if best < score:
-                    best = score
-            scores.append(score)  # najlepszy wynik dla każdej cechy
-            print(scores)
-        results.append(max(scores))  # najlepszy wynik dla każdego z klasyfikatorów
-        print(results)
+                scores.append(accuracy_score(y_test, predict))  # wynik każdego ksperymentu w ramach jednego foldu
+            mean = np.mean(scores)  # średnia z ostatnich foldów dla danego klasyfikatora z daną liczna cech
+            results[j, i-1] = mean
+            print(results)
+            j = j + 1
 
-    np.save('results', scores)
+    np.save('results', results)
     # analiza t-studenta
 
     alfa = .05
     t_statistic = np.zeros((6, 6))
     p_value = np.zeros((6, 6))
+    s = np.array(results)
 
-    for i in range(0, 6):
-        for j in range(0, 6):
-            t_statistic[i, j], p_value[i, j] = ttest_ind(results[i], results[j])
-    print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
+    #for i in range(0, 6):
+    #    for j in range(0, 6):
+    #        t_statistic[i, j], p_value[i, j] = ttest_rel(s[i], s[j])
+    #print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
 
-    #plt.figure(figsize=(15, 8))
-    #plt.style.use("ggplot")
-    #plt.barh(range(len(r)), [s[1] for s in r], align='center')
-    #plt.yticks(range(len(r)), [s[0] for s in r])
-    #plt.title('Ranking cech')
-    #plt.rc('ytick', labelsize=14)
-    #plt.show
-    #plt.savefig('plot.png')
+    # plt.figure(figsize=(15, 8))
+    # plt.style.use("ggplot")
+    # plt.barh(range(len(r)), [s[1] for s in r], align='center')
+    # plt.yticks(range(len(r)), [s[0] for s in r])
+    # plt.title('Ranking cech')
+    # plt.rc('ytick', labelsize=14)
+    # plt.show
+    # plt.savefig('plot.png')
